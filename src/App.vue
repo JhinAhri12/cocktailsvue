d37662de18a453e96f1177f86e9038819af300a4
JhinAhri12 <cyprien.rezki@hotmail.fr>
2021-07-21 20:22:07 +0200

    axios : ok teamplate : débuté api : ok

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Cocktails />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cocktails from './components/Cocktails.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Cocktails
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Cocktails.vue
@@ -0,0 +1,39 @@
+<template>
+
+  <h1> Films:</h1>
+  <h4> suite a un soucis d'api il y aura des films a la place des cocktails. </h4>
+  <h3>{{cocktails.title}}</h3>
+  <img :src="cocktails.poster" class="rounded img-thumbnail">
+
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Cocktails',
+  data() {
+    return {
+      cocktails: null,
+    };
+  },
+  created: function() {
+
+    axios
+      .get('https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666', { headers: {
+        'x-rapidapi-key': '5ee4b09894mshb2a3ba1fd076540p1358c3jsnbe47eb0a4c5b',
+        'x-rapidapi-host': 'imdb-internet-movie-database-unofficial.p.rapidapi.com' }
+  })
+      .then(res => {
+        this.cocktails = res.data;
+        console.log(res.data);
+      })
+  }
+}
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
